f3130de2b580dcb322e76bafa9df9b83dbbd090b
Joy Iyosayi <airhunmwundejoy@gmail.com>
2025-04-04 18:50:57 +0100

    Update football_api_s3.py

--- s3_projects/football_api_s3.py
+++ s3_projects/football_api_s3.py
@@ -1,1 +1,47 @@
+import awswrangler as wr
+import pandas as pd
+from dotenv import load_dotenv
+import boto3
+import os
+import requests
+
+# Load environment variables
+load_dotenv()
+
+url = "http://api.football-data.org/v4/competitions/"
+
+response_API = requests.get(url)
+
+football_data = response_API.json()
+
+football_data['competitions']
+
+competition_names_list = []
+
+for competition in football_data['competitions']:
+    name = competition['name']
+    competition_names_list.append(name)
+
+distinct_competition_names = set(competition_names_list)
+
+competition_names = list(set(competition_names_list))
+
+# Normalize the 'jobs_list' JSON data into a DataFrame
+df = pd.json_normalize(competition_names)
+
+# Create a boto3 session
+session = boto3.Session(
+    aws_access_key_id=os.getenv("ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+    region_name='eu-north-1'
+)
+
+# Save DataFrame as a Parquet file in S3 bucket
+wr.s3.to_parquet(
+    df=df,
+    path="s3://joy-airhunmwunde-bucket/competition_names.parquet",
+    dataset=True,
+    mode='append',
+    boto3_session=session
+)
 
